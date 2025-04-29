fb023d217fc76317d3a691dd8be4446c1d0728c6
Bea <ab291012@gmail.com>
2025-04-29 08:36:56 -0300

    feat: add libs and schema.sql

---
+++ src/database/schema.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE cosmeticos;
+
+\c cosmeticos
+
+CREATE TABLE marcas (
+    id SERIAL PRIMARY KEY,
+    marca VARCHAR (100) NOT NULL,
+    founder VARCHAR(50) NOT NULL
+);
+
+SELECT * FROM marcas;
+
+CREATE TABLE cosmeticos (
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(200) NOT NULL,
+    price VARCHAR (30) NOT NULL,
+    amount VARCHAR (50) NOT NULL,
+    marca_id INTEGER REFERENCES marcas(id) ON DELETE CASCADE
+);
+
+SELECT * FROM cosmeticos;
+
+INSERT INTO marcas (marca, founder) VALUES
+('Wella', 'Franz Ströher'),
+('Lola Cosmetics', 'Dione Vasconcellos'),
+('Creamy', 'Gabriel Beleze'),
+('Kiko Milano', 'Antonio Percassi');
+
+INSERT INTO cosmeticos (type, price, amount, marca_id) VALUES
+('Kit Wella Professionals Invigo Nutri-Enrich', 'R$382,00', '1 Litro', 1),
+('Lola Óleo Argan Oil', 'R$14,99', '10ml', 2),
+('Creme Hidratante Facial Creamy', 'R$47,98', '40g', 3),
+('Base Full Coverage 2 In 1 Kiko Milano', 'R$139,90', '50ml', 4);
